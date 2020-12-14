6d0ce0933e9f9b172745a432cb22adb59b512664
Nico Lauria <nflauria@outlook.com>
2020-12-13 16:00:58 -0800

    basic express server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`app running on PORT ${PORT}`)
+})
+
+// set up mongoose
+const uri = process.env.MONGODB_ATLAS_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
